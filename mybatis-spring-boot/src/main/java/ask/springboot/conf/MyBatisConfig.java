45185000bbaf64d67aa49c3ec458fcd17c82c923
peng <cenglou99@gmail.com>
2016-08-02 17:16:28 +0800

    a

--- mybatis-spring-boot/src/main/java/ask/springboot/conf/MyBatisConfig.java
+++ mybatis-spring-boot/src/main/java/ask/springboot/conf/MyBatisConfig.java
@@ -54,7 +54,7 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
     public SqlSessionFactory sqlSessionFactoryBean() {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
-        bean.setTypeAliasesPackage("ask.mybatis.springboot.model");
+        bean.setTypeAliasesPackage("ask.springboot.model");
 
         //分页插件
         PageHelper pageHelper = new PageHelper();
