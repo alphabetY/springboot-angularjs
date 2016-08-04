d35880b0679deca5b4bfcb66021933c28f22db03
peng <cenglou99@gmail.com>
2016-08-04 15:20:32 +0800

    security

---
+++ src/main/java/ask/springboot/security/Security.java
@@ -0,0 +1,25 @@
+// Copyright (c) 2014 KMS Technology, Inc.
+package ask.springboot.security;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author trungnguyen
+ */
+@Order(Ordered.LOWEST_PRECEDENCE - 8)
+public class Security extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic().and()
+            .csrf().disable()
+            .authorizeRequests()
+            .antMatchers(HttpMethod.GET, "/rest/contacts*/**").permitAll()
+            .antMatchers(HttpMethod.GET, "/rest/photos*/**").permitAll()
+            .antMatchers("/rest/**").authenticated()
+            .anyRequest().permitAll();
+    }
+}
