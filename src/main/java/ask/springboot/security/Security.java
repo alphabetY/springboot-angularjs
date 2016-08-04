a0768b181eea4401cd1f82bc74af98522df0d57d
peng <cenglou99@gmail.com>
2016-08-04 15:32:46 +0800

    a

--- src/main/java/ask/springboot/security/Security.java
+++ src/main/java/ask/springboot/security/Security.java
@@ -1,9 +1,11 @@
 // Copyright (c) 2014 KMS Technology, Inc.
 package ask.springboot.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -12,7 +14,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Order(Ordered.LOWEST_PRECEDENCE - 8)
 public class Security extends WebSecurityConfigurerAdapter {
-    @Override
+    
+	
+	
+	
+	    @Override
+	    @Bean
+	    public UserDetailsService userDetailsService() {
+	        return new UserDetailsService();
+	    }
+
+	    @Override  
+	    protected void configure(AuthenticationManagerBuilder auth)  
+	            throws Exception {  
+	        auth.userDetailsService(userDetailsService());  
+	    }  
+	
+	@Override
     protected void configure(HttpSecurity http) throws Exception {
         http.httpBasic().and()
             .csrf().disable()
